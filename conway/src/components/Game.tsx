91ee777c9030ca40dd0daae9d911d76473398e57
BaoPham92 <baopham92@yahoo.com>
2020-10-16 18:44:48 -0400

    code cleanup

--- conway/src/components/Game.tsx
+++ conway/src/components/Game.tsx
@@ -1,39 +1,47 @@
 import React, { useState } from 'react';
 import "./styles/Game.css"
 
-const gridTemplate = (
-    rowNumber = 100,
-    colNumber = 100,
-    size = 100
-) => Array(size).fill(0).map(() => Array(size).fill(0))
+const gridTemplate = (size: number = 100) => Array(size).fill(0).map(() => Array(size).fill(0));
+const rowNumber = 100;
+const colNumber = 100;
 
 const Game: React.FC = () => {
     const [grid, setGrid] = useState(() => gridTemplate());
+
+    const updateCell = (
+        grid: any, 
+        rIndex: any, 
+        cIndex: any
+        ) => {
+
+        // * NEW GRID COPY
+        const newGrid = [...grid];
+        
+        // * UPDATE SPECIFIC INDEX'S
+        if (!grid[rIndex][cIndex]) {
+            grid[rIndex][cIndex] = 1
+        } else {
+            grid[rIndex][cIndex] = 0
+        }
+        
+        // * UPDATE GRID STATE
+        newGrid[rIndex] = grid[rIndex];
+        setGrid(newGrid);
+    }
 
     return (
         <main className="grid__container">
             {
                 grid.map((rows, rIndex) => rows.map(
-                    (col, cIndex) => <div
-                        key={rIndex + cIndex}
-                        onClick={() => {
-                            const newGrid = [...grid];
-
-                            if (!grid[rIndex][cIndex]) {
-                                grid[rIndex][cIndex] = 1
-                            } else {
-                                grid[rIndex][cIndex] = 0
-                            }
-
-                            newGrid[rIndex] = grid[rIndex];
-                            setGrid(newGrid);
-                        }}
-                        style={{
-                            border: "solid 1px black",
-                            backgroundColor: grid[rIndex][cIndex] ? "blue" : "white",
-                            width: 10,
-                            height: 10,
-                        }}>
+                    (col, cIndex) => <div 
+                    key={rIndex + cIndex} 
+                    onClick={() => updateCell(grid, rIndex, cIndex)}
+                    style={{
+                        border: "solid 1px black",
+                        backgroundColor: grid[rIndex][cIndex] ? "blue" : "white",
+                        width: 10,
+                        height: 10,
+                    }}>
                     </div>
                 ))
             }
