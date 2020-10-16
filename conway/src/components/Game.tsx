cbcee54bfc5342a2b584a451e1c99bb409415325
BaoPham92 <baopham92@yahoo.com>
2020-10-16 18:45:56 -0400

    logic for simulation

--- conway/src/components/Game.tsx
+++ conway/src/components/Game.tsx
@@ -1,51 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./styles/Game.css"
 
 const gridTemplate = (size: number = 100) => Array(size).fill(0).map(() => Array(size).fill(0));
-const rowNumber = 100;
-const colNumber = 100;
+const rowNumber: number = 100;
+const colNumber: number = 100;
 
 const Game: React.FC = () => {
     const [grid, setGrid] = useState(() => gridTemplate());
+    const [gameState, setGameState] = useState(false);
+
+    useEffect(() => {
+        initiateSim(grid);
+    }, [gameState])
 
     const updateCell = (
-        grid: any, 
-        rIndex: any, 
+        grid: any,
+        rIndex: any,
         cIndex: any
-        ) => {
+    ) => {
 
         // * NEW GRID COPY
         const newGrid = [...grid];
-        
+
         // * UPDATE SPECIFIC INDEX'S
         if (!grid[rIndex][cIndex]) {
             grid[rIndex][cIndex] = 1
         } else {
             grid[rIndex][cIndex] = 0
         }
-        
+
         // * UPDATE GRID STATE
         newGrid[rIndex] = grid[rIndex];
         setGrid(newGrid);
     }
 
-    return (
-        <main className="grid__container">
-            {
-                grid.map((rows, rIndex) => rows.map(
-                    (col, cIndex) => <div 
-                    key={rIndex + cIndex} 
-                    onClick={() => updateCell(grid, rIndex, cIndex)}
-                    style={{
-                        border: "solid 1px black",
-                        backgroundColor: grid[rIndex][cIndex] ? "blue" : "white",
-                        width: 10,
-                        height: 10,
-                    }}>
-                    </div>
-                ))
+    const initiateSim = (grid: any) => {
+
+        // * NEW GRID COPY
+        const newGrid = [...grid];
+
+        const neighborCheck = [
+            [0, 1],
+            [0, -1],
+            [1, 0],
+            [-1, 0],
+            [1, -1],
+            [-1, 1],
+            [1, 1],
+            [-1, -1]
+        ];
+
+        if (gameState === true) {
+            for (let i = 0; i < rowNumber; i++) {
+                for (let c = 0; c < colNumber; c++) {
+                    let cellState = 0;
+                    neighborCheck.forEach(([x, y]) => {
+                        const positionX = i + x;
+                        const positionY = c + y;
+
+                        if (
+                            positionX >= 0 &&
+                            positionX < rowNumber &&
+                            positionY >= 0 &&
+                            positionY < colNumber
+                        ) cellState += grid[positionX][positionY];
+                    })
+
+                    if (cellState < 2 || cellState > 3) {
+                        newGrid[i][c] = 0;
+                    } else if (grid[i][c] === 0 && cellState === 3) {
+                        newGrid[i][c] = 1;
+                    }
+                }
             }
-        </main>
+
+            setGrid(newGrid);
+        }
+
+        setTimeout(() => initiateSim(grid), 1000);
+    }
+
+    return (
+        <>
+            <button
+                onClick={() => {setGameState(!gameState)}}>{!gameState ? "start" : "stop"}</button>
+            <main className="grid__container">
+                {
+                    grid.map((rows, rIndex) => rows.map(
+                        (col, cIndex) => <div
+                            key={rIndex + cIndex}
+                            onClick={() => updateCell(grid, rIndex, cIndex)}
+                            style={{
+                                border: "solid 1px black",
+                                backgroundColor: grid[rIndex][cIndex] ? "blue" : "white",
+                                width: 10,
+                                height: 10,
+                            }}>
+                        </div>
+                    ))
+                }
+            </main>
+        </>
     );
 }
 
